f4a483a404da127bf606d26487c3712bad46f852
encyk <martiiin225@email.cz>
2022-03-15 23:11:49 +0100

    done color ui, added spoiler customization, created mod icons

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -7,26 +7,18 @@ public class CameraController : MonoBehaviour
     [SerializeField] Transform cameraSpots;
     int selectedSpot = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             selectedSpot = (selectedSpot == cameraSpots.childCount - 1) ? 0 : selectedSpot + 1;
-            transform.position = cameraSpots.GetChild(selectedSpot).position;
-            transform.rotation = cameraSpots.GetChild(selectedSpot).rotation;
+            transform.SetPositionAndRotation(cameraSpots.GetChild(selectedSpot).position, cameraSpots.GetChild(selectedSpot).rotation);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             selectedSpot = (selectedSpot == 0) ? cameraSpots.childCount - 1 : selectedSpot - 1;
-            transform.position = cameraSpots.GetChild(selectedSpot).position;
-            transform.rotation = cameraSpots.GetChild(selectedSpot).rotation;
+            transform.SetPositionAndRotation(cameraSpots.GetChild(selectedSpot).position, cameraSpots.GetChild(selectedSpot).rotation);
         }
     }
 }
--- Assets/Konfigurator.cs
+++ Assets/Konfigurator.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Konfigurator : MonoBehaviour
 {
+    [System.Serializable] public struct Mod
+    {
+        public Texture Icon;
+        public GameObject Model;
+        public Material? ColorMaterial;
+    }
+
     [SerializeField] Material carMaterial;
     [SerializeField] Color[] bodyColors;
     [SerializeField] GameObject carBody;
@@ -12,13 +20,37 @@ public class Konfigurator : MonoBehaviour
     [SerializeField] Transform[] wheelPositions;
     int _wheelIndex;
 
+    [SerializeField] HorizontalLayoutGroup colorsPanel;
+    [SerializeField] GameObject colorButtonPrefab;
+
+    [SerializeField] Mod[] wheels;
+    [SerializeField] Mod[] spoilers;
+    [SerializeField] Transform spoilerHolder;
+    int selectedSpoilerIndex;
+
 
     int _i = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        //celou tuhle logiku prolly presunout do samostatneho skriptu, ktery se bude starat o ui
 
+        //init buttons
+        foreach (Color color in bodyColors)
+        {
+            GameObject button = Instantiate(colorButtonPrefab);
+            button.GetComponent<RectTransform>().sizeDelta = new Vector2(50, 62);
+            button.transform.SetParent(colorsPanel.transform, false);
+            button.GetComponent<Image>().color = color;
+            button.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                ChangeColor(color);
+            });
+        }
+
+
+        //todo: nastavit vsem modum a autu stejnou barvu 
     }
 
     // Update is called once per frame
@@ -38,6 +70,28 @@ public class Konfigurator : MonoBehaviour
 
             ChangeWheels(_wheelIndex);
         }
+
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            selectedSpoilerIndex = (selectedSpoilerIndex == spoilers.Length - 1) ? 0 : selectedSpoilerIndex + 1;
+            ChangeSpoiler(selectedSpoilerIndex);
+        }
+    }
+
+    public void ChangeSpoiler(int spoilerIndex)
+    {
+        foreach(Transform spoiler in spoilerHolder) //asi neni idealni, ale na pokud bude potreba optimalizace, tak to nejak pujde. Pokud presunu UI 
+                                                    //do samostatne classtky, tak si tu musu necht promennou lastSpoiler a podle te to budu vypinat.
+                                                    //tohle je asi hlavne rychlej prototyp
+        {
+            spoiler.gameObject.SetActive(false);
+        }
+        Debug.Log(spoilers[spoilerIndex].Model.activeSelf);
+        spoilers[selectedSpoilerIndex].Model.SetActive(true);
+        Debug.Log(spoilers[spoilerIndex].Model.name);
+        Debug.Log(spoilers[spoilerIndex].Model.activeSelf);
+
+        //todo: ejak poresit obarvovani spoileru //probs si ukladat barvu na kterou bylo posledne zmeneno a tady zavolat ChangeColor(ta barva).
     }
 
     public void ChangeWheels(int wheelIndex) //mozna ne pres index? (-_-)?  //a tohle je prej "scratching head" emoticon
@@ -57,6 +111,9 @@ public class Konfigurator : MonoBehaviour
 
     public void ChangeColor(Color c)
     {
+
+
         carMaterial.color = c;
+        if(spoilers[selectedSpoilerIndex].ColorMaterial != null) spoilers[selectedSpoilerIndex].ColorMaterial.color = c;
     }
 }
